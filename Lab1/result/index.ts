e56b45ef68cecac28b295250cee3a5a00c93f36c
nanotronic <nanotron@drehmoment.org>
2023-01-13 09:26:06 +0000

    second

--- Lab1/result/index.ts
+++ Lab1/result/index.ts
@@ -1,1 +1,11 @@
 import * as pulumi from "@pulumi/pulumi";
+
+const config = new pulumi.Config();
+
+const myname = config.require('myname');
+const mysecret = config.requireSecret('mysecret');
+
+const optionalParam = config.get('optional') || 'defaultValue';
+
+export const name = myname;
+export const secret = mysecret;
